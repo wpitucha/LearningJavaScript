9086e914d53b9912e64b814c8bf435f8894c7769
Wojciech <69200642+ImMrRobot@users.noreply.github.com>
2020-09-03 19:04:21 +0200

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-Wait for it...
+function timer()
+{
+    let today = new Date();
+
+    let dzien = today.getDate();
+    let miesiac = today.getMonth();
+    let rok = today.getFullYear();
+
+    let godzina = today.getHours();
+    if (godzina<10)  godzina = '0' + godzina;
+    let minuta = today.getMinutes();
+    if (minuta<10) minuta = '0' + minuta;
+    let sekunda = today.getSeconds();
+    if (sekunda<10) sekunda = '0' + sekunda;
+
+    document.getElementById('zegar').innerHTML=
+    godzina+':'+minuta+':'+sekunda;
+
+    setTimeout('timer()',1000);
+}
